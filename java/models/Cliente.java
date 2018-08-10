fa2e24f5d0d6c9ce5ddf15b71e54e052d73e2cd8
joao_felipe <joaofelipe1294@gmail.com>
2018-08-10 01:12:43 -0300

    Adicionado atributo salario a classe Cliente

--- java/models/Cliente.java
+++ java/models/Cliente.java
@@ -6,4 +6,43 @@ public class Cliente {
 	private String sobrenome;
 	private String rg;
 	private String cpf;
+	private double salario;
+	
+	public long getClienteId() {
+		return clienteId;
+	}
+	public void setClienteId(long clienteId) {
+		this.clienteId = clienteId;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
 }
