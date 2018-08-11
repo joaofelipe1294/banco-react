4bdbb47ba9570eb712bd1abeca3e74818b0b51ea
joao_felipe <joaofelipe1294@gmail.com>
2018-08-11 01:40:43 -0300

    Adicionada navbar da aplicacao

--- banco_react/src/App.js
+++ banco_react/src/App.js
@@ -1,40 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-//import './App.css';
-import './css/pure-min.css';
-import './css/side-menu.css';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import 'bootstrap'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div id="layout">
-            <a href="#menu" id="menuLink" class="menu-link">
-                <span></span>
-            </a>
-            <div id="menu">
-                <div class="pure-menu">
-                    <a class="pure-menu-heading" href="#">Company</a>
-                    <ul class="pure-menu-list">
-                        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Home</a></li>
-                        <li class="pure-menu-item"><a href="#" class="pure-menu-link">About</a></li>
-                        <li class="pure-menu-item menu-item-divided pure-menu-selected">
-                            <a href="#" class="pure-menu-link">Services</a>
-                        </li>
-                        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Contact</a></li>
-                    </ul>
-                </div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                  </a>
+                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a className="dropdown-item" href="#">Action</a>
+                    <a className="dropdown-item" href="#">Another action</a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item" href="#">Something else here</a>
+                  </div>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link disabled" href="#">Disabled</a>
+                </li>
+              </ul>
+              <form className="form-inline my-2 my-lg-0">
+                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
             </div>
-            <div id="main">
-              <div class="header">
-                <h1>Page Title</h1>
-                <h2>A subtitle for your page goes here</h2>
-              </div>
-              <div class="content">
-              </div>
-            </div>
-        </div>
+          </nav>
       </div>
     );
   }
