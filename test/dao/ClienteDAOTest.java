56168f7a1cfcd2bad0fd1390f98433fcd28a759d
joao_felipe <joaofelipe1294@gmail.com>
2018-08-10 23:43:10 -0300

    Adicionados testes que validam parametros repetidos no cadasteo de novo cliente

--- test/dao/ClienteDAOTest.java
+++ test/dao/ClienteDAOTest.java
@@ -30,7 +30,7 @@ public class ClienteDAOTest {
 	}
 	
 	@Test
-	public void test() throws SQLException {
+	public void teste_cadastro_valido() throws SQLException {
 		Cliente cliente = new Cliente();
 		cliente.setNome("Jon");
 		cliente.setSobrenome("Doe");
@@ -39,5 +39,55 @@ public class ClienteDAOTest {
 		cliente.setSalario(4900.0);
 		dao.cadastra(cliente);
 	}
+	
+	@Test(expected = RuntimeException.class)
+	public void teste_cadastro_invalido_rg_e_cpf_duplicados() throws SQLException {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Jon");
+		cliente.setSobrenome("Doe");
+		cliente.setRg("009900");
+		cliente.setCpf("828292825");
+		cliente.setSalario(4900.0);
+		dao.cadastra(cliente);
+		dao.cadastra(cliente);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void teste_cadastro_invalido_cpf_duplicado() {
+		Cliente cliente1 = new Cliente();
+		cliente1.setNome("Jon");
+		cliente1.setSobrenome("Doe");
+		cliente1.setRg("009900");
+		cliente1.setCpf("828292825");
+		cliente1.setSalario(4900.0);
+		Cliente cliente2 = new Cliente();
+		cliente2.setNome("Zé");
+		cliente2.setSobrenome("da Silva");
+		cliente2.setRg("1234");
+		cliente2.setCpf("828292825");
+		cliente2.setSalario(1900.0);
+		dao.cadastra(cliente1);
+		dao.cadastra(cliente2);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void teste_cadastro_invalido_rg_duplicado() {
+		Cliente cliente1 = new Cliente();
+		cliente1.setNome("Jon");
+		cliente1.setSobrenome("Doe");
+		cliente1.setRg("009900");
+		cliente1.setCpf("828292825");
+		cliente1.setSalario(4900.0);
+		Cliente cliente2 = new Cliente();
+		cliente2.setNome("Zé");
+		cliente2.setSobrenome("da Silva");
+		cliente2.setRg("009900");
+		cliente2.setCpf("1234");
+		cliente2.setSalario(1900.0);
+		dao.cadastra(cliente1);
+		dao.cadastra(cliente2);
+	}
+	
+	
 
 }
