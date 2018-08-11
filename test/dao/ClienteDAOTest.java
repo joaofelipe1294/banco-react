c6fe6d7bca8c2e60640fca93409e03c254f63097
joao_felipe <joaofelipe1294@gmail.com>
2018-08-10 23:36:06 -0300

    Refatorado código que testa o cadastro de um novo cliente

--- test/dao/ClienteDAOTest.java
+++ test/dao/ClienteDAOTest.java
@@ -3,25 +3,41 @@ package dao;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import connection.ConnectionFactory;
+import helpers.LimpaClientesHelper;
 import models.Cliente;
 
 public class ClienteDAOTest {
-
+	private Connection connection;
+	private ClienteDAO dao;
+	
+	
+	@Before
+	public void before() {
+		this.connection = new ConnectionFactory().getConnection();
+		new LimpaClientesHelper(connection).limpa();
+		this.dao = new ClienteDAO(connection);
+	}
+	
+	@After
+	public void after() throws SQLException {
+		this.connection.rollback();
+		this.connection.close();
+	}
+	
 	@Test
 	public void test() throws SQLException {
-		Connection connection = new ConnectionFactory().getConnection();
 		Cliente cliente = new Cliente();
 		cliente.setNome("Jon");
 		cliente.setSobrenome("Doe");
 		cliente.setRg("009900");
 		cliente.setCpf("828292825");
 		cliente.setSalario(4900.0);
-		new ClienteDAO(connection).cadastra(cliente);
-		connection.commit();
-		connection.close();
+		dao.cadastra(cliente);
 	}
 
 }
