58ebda22baa5d4e5b21b42eb9726d8309e412ae4
joao_felipe <joaofelipe1294@gmail.com>
2018-08-11 01:03:22 -0300

    Adicionada importacao da lib css Pure e aplicado o layput na pagina index

--- banco_react/src/App.js
+++ banco_react/src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import './css/pure-min.css';
+import './css/side-menu.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div id="layout">
+            <a href="#menu" id="menuLink" class="menu-link">
+                <span></span>
+            </a>
+            <div id="menu">
+                <div class="pure-menu">
+                    <a class="pure-menu-heading" href="#">Company</a>
+                    <ul class="pure-menu-list">
+                        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Home</a></li>
+                        <li class="pure-menu-item"><a href="#" class="pure-menu-link">About</a></li>
+                        <li class="pure-menu-item menu-item-divided pure-menu-selected">
+                            <a href="#" class="pure-menu-link">Services</a>
+                        </li>
+                        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div id="main">
+              <div class="header">
+                <h1>Page Title</h1>
+                <h2>A subtitle for your page goes here</h2>
+              </div>
+              <div class="content">
+              </div>
+            </div>
+        </div>
       </div>
     );
   }
