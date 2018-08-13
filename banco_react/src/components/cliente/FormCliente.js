86fdc980c26ed8e992e83bf49cd7f0972fd23c48
joao_felipe <joaofelipe1294@gmail.com>
2018-08-13 09:38:57 -0300

    ALterada implementação do design do formulario de cadastro de cliente para que use o componente que abstrai um formGroup

--- banco_react/src/components/cliente/FormCliente.js
+++ banco_react/src/components/cliente/FormCliente.js
@@ -1,48 +1,34 @@
 import React, { Component } from 'react';
+import FormGroupGenerico from "../genericos/FormGroupGenerico";
 
 export class FormCliente extends Component{
     render(){
         return (
             <div className = "col-md-9 mt-4 mx-auto">
-            <div className="card">
-              <div className="card-header text-center bg-primary">
-                <h1 className = "text-white">Cadastro de cliente</h1>
-              </div>
-              <div className="card-body">
-                <form>
-                  <div className = "row">
-                    <div className = "col-md-6">
-                      <div className="form-group">
-                        <label htmlFor="nome">Nome</label>
-                        <input className="form-control" id="nome"/>
-                      </div>
-                      <div className="form-group">
-                        <label htmlFor="RG">RG</label>
-                        <input className="form-control" id="rg"/>
-                      </div>
-                      <div className="form-group">
-                      <label htmlFor="salario">Salário</label>
-                      <input className="form-control" id="salario" type="number" step="0.01"/>
+                <div className="card">
+                    <div className="card-header text-center bg-primary">
+                        <h1 className = "text-white">Cadastro de cliente</h1>
                     </div>
+                    <div className="card-body">
+                        <form>
+                            <div className = "row">
+                                <div className = "col-md-6">
+                                    <FormGroupGenerico name = "Nome" id = "nome"/>
+                                    <FormGroupGenerico name = "RG" id = "rg" type = "number"/>
+                                    <FormGroupGenerico name = "Salário" id = "salario" type = "number" step = "0.01"/>
+                                </div>
+                                <div className = "col-md-6">
+                                    <FormGroupGenerico name = "Sobrenome" id = "sobrenome"/>
+                                    <FormGroupGenerico name = "CPF" id = "cpf" type = "number"/>
+                                </div>
+                            </div>
+                            <div className = "form-group">
+                                <button className = "btn btn-primary">Cadastrar</button>
+                            </div>
+                        </form>
                     </div>
-                    <div className = "col-md-6">
-                      <div className="form-group">
-                        <label htmlFor="sobrenome">Sobrenome</label>
-                        <input className="form-control" id="sobrenome"/>
-                      </div>
-                      <div className="form-group">
-                        <label htmlFor="cpf">CPF</label>
-                        <input className="form-control" id="nome"/>
-                      </div>
-                    </div>
-                  </div>
-                  <div className = "form-group">
-                    <button className = "btn btn-primary">Cadastrar</button>
-                  </div>
-                </form>
-              </div>
+                </div>
             </div>
-          </div>
         );
     }
 
