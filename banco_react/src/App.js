ee8adb4202c6124659a6cb188046c751bd0c9c02
joao_felipe <joaofelipe1294@gmail.com>
2018-08-13 09:03:17 -0300

    Concluido HTML do form que cadastra um novo cliente

--- banco_react/src/App.js
+++ banco_react/src/App.js
@@ -7,40 +7,66 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">Banco-React</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item active">
-                  <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                  <a className="nav-link" href="#">Cliente <span className="sr-only">(current)</span></a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link disabled" href="#">Disabled</a>
+                  <a className="nav-link" href="#">Conta</a>
                 </li>
               </ul>
-              <form className="form-inline my-2 my-lg-0">
-                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-              </form>
             </div>
           </nav>
+
+          {/* AQUI COMECA CODIGO DO CLIENTE */}
+          <div className = "col-md-9 mt-4 mx-auto">
+            <div className="card">
+              <div className="card-header text-center bg-primary">
+                <h1 className = "text-white">Cadastro de cliente</h1>
+              </div>
+              <div className="card-body">
+                <form>
+                  <div className="form-group">
+                    <label htmlFor="nome">Nome</label>
+                    <input className="form-control" id="nome"/>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="sobrenome">Sobrenome</label>
+                    <input className="form-control" id="sobrenome"/>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="RG">RG</label>
+                    <input className="form-control" id="rg"/>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="cpf">CPF</label>
+                    <input className="form-control" id="nome"/>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="salario">Salário</label>
+                    <input className="form-control" id="salario" type="number" step="0.01"/>
+                  </div>
+                  <div className = "form-group">
+                    <button className = "btn btn-primary">Cadastrar</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+
+
+
+
+
+
+
+
       </div>
     );
   }
