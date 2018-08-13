9d325fa15a1dc4fa5553943a17feaf6022388070
joao_felipe <joaofelipe1294@gmail.com>
2018-08-13 09:10:00 -0300

    Alterado layout do formulario que cadastra um novo cliente

--- banco_react/src/App.js
+++ banco_react/src/App.js
@@ -32,25 +32,31 @@ class App extends Component {
               </div>
               <div className="card-body">
                 <form>
-                  <div className="form-group">
-                    <label htmlFor="nome">Nome</label>
-                    <input className="form-control" id="nome"/>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="sobrenome">Sobrenome</label>
-                    <input className="form-control" id="sobrenome"/>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="RG">RG</label>
-                    <input className="form-control" id="rg"/>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="cpf">CPF</label>
-                    <input className="form-control" id="nome"/>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="salario">Salário</label>
-                    <input className="form-control" id="salario" type="number" step="0.01"/>
+                  <div className = "row">
+                    <div className = "col-md-6">
+                      <div className="form-group">
+                        <label htmlFor="nome">Nome</label>
+                        <input className="form-control" id="nome"/>
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="RG">RG</label>
+                        <input className="form-control" id="rg"/>
+                      </div>
+                      <div className="form-group">
+                      <label htmlFor="salario">Salário</label>
+                      <input className="form-control" id="salario" type="number" step="0.01"/>
+                    </div>
+                    </div>
+                    <div className = "col-md-6">
+                      <div className="form-group">
+                        <label htmlFor="sobrenome">Sobrenome</label>
+                        <input className="form-control" id="sobrenome"/>
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="cpf">CPF</label>
+                        <input className="form-control" id="nome"/>
+                      </div>
+                    </div>
                   </div>
                   <div className = "form-group">
                     <button className = "btn btn-primary">Cadastrar</button>
