e534aab99ef92ffaea15f9762b6032e6bd20f185
joao_felipe <joaofelipe1294@gmail.com>
2018-08-14 15:00:51 -0300

    Adicionado código base do webService que irá tratar dos clientes

--- java/webServices/ClienteWS.java
+++ java/webServices/ClienteWS.java
@@ -1,11 +1,16 @@
 package webServices;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+
+import com.google.gson.Gson;
 
 import models.Cliente;
 
@@ -13,22 +18,31 @@ import models.Cliente;
 public class ClienteWS {
 
 	@Context
-    private UriInfo context;
+	private UriInfo context;
 
 	public ClienteWS() {
 	}
-	
-	@POST
-	public Response cadastra(Cliente cliente) {
-		return Response.ok().build();
-	}
-	
-	@GET
-	public Response lista() {
-		return Response.ok("Deu boa !").build();
-	}
-	
 
-	
-	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response cadastra(Cliente cliente) {
+		System.out.println(cliente.getNome());
+		String resposta = "DEU BOA";
+		return Response
+				.ok(new Gson().toJson(resposta))
+				.build();
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response lista() {
+		System.out.println("YO ");
+		Cliente cliente = new Cliente();
+		cliente.setNome("Nate Diaz");
+		return Response
+				.ok(new Gson().toJson(cliente))
+				.build();
+	}
+
 }
