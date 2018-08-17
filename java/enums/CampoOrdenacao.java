45318801df60f256902ca8abb4e7df3e89279d18
joao_felipe <joaofelipe1294@gmail.com>
2018-08-17 09:27:51 -0300

    Adicionada enum que abstrái os tipos de listagem permitidos

---
+++ java/enums/CampoOrdenacao.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enums;
+
+/**
+ *
+ * @author joaofelipe
+ */
+public enum CampoOrdenacao {
+
+    NOME , SOBORENOME , SALARIO;
+        
+    //private final String tipo;
+
+    //private CampoOrdenacao(String tipo) {
+    //    this.tipo = tipo;
+    //}
+
+
+    //public String getTipo() {
+    //    return tipo;
+    //}
+    
+}
