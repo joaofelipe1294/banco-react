c8513e47e978bd7453cd0362e2ff7c10ef63c18f
joao_felipe <joaofelipe1294@gmail.com>
2018-08-17 21:59:19 -0300

    Adicionada alteração para que o layout principal seja reaproveitado nas demais rotas

--- banco_react/src/App.js
+++ banco_react/src/App.js
@@ -4,8 +4,8 @@ import $ from 'jquery';
 import 'bootstrap'
 import ClienteBox from './components/cliente/ClienteBox';
 import { library } from '@fortawesome/fontawesome-svg-core'
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import {Link} from 'react-router-dom';
 
 library.add(fas);
 
@@ -14,14 +14,14 @@ class App extends Component {
     return (
       <div className="App">
           <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">Banco-React</a>
+            <Link className="navbar-brand" to={'/'}>Banco-React</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item active">
-                  <a className="nav-link" href="#">Cliente <span className="sr-only">(current)</span></a>
+                  <Link className="nav-link" to={'/cliente'}>Cliente</Link>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="#">Conta</a>
@@ -32,9 +32,9 @@ class App extends Component {
 
           {/* AQUI COMECA CODIGO DO CLIENTE */}
           
-          <ClienteBox/>
+          {/*<ClienteBox/>*/}
 
-
+          {this.props.children}
 
         { /* ACABA AQUI A DIV MAIN */}
       </div>
