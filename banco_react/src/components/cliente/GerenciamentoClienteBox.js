b99b15c35471db647da2ec805d1e6a3085adbd4a
joao_felipe <joaofelipe1294@gmail.com>
2018-08-18 09:18:21 -0300

    Adicionado componente que irá controlar as ações na rota /clientes, terá todas as manipulações referentes a busca, edição e exclusão de cliente

---
+++ banco_react/src/components/cliente/GerenciamentoClienteBox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import FormBuscaCliente from './FormBuscaCliente';
+
+export default class GerenciamentoClienteBox extends Component{
+
+    render(){
+        return (
+
+
+        );
+    }
+
+
+}
