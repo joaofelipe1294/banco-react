7f285b0c411f8fa6c82a43b5e8b89858a0b85b63
joao_felipe <joaofelipe1294@gmail.com>
2018-08-18 09:13:07 -0300

    Corrigidos bugs referentes ao uso de class e não className no HTML

--- banco_react/src/components/genericos/FormGroupPesquisaGenerico.js
+++ banco_react/src/components/genericos/FormGroupPesquisaGenerico.js
@@ -6,7 +6,7 @@ export default class FormGroupPesquisaGenerico extends Component{
 
     render(){
         return(
-            <div class="input-group mb-3">
+            <div className="input-group mb-3">
                 <input className="form-control" placeholder={this.props.label} aria-label={this.props.label} type = {this.props.type} aria-describedby="basic-addon2"/>
                 <div className="input-group-append">
                     <button className="btn btn-outline-info" type="button" onClick = {this.props.onClick}>
