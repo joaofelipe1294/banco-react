9a60e1ed05b8274fd871ebc18ccd70cde83c79e0
joao_felipe <joaofelipe1294@gmail.com>
2018-08-17 22:00:00 -0300

    Adicionadas primairas rotas do sistema

--- banco_react/src/index.js
+++ banco_react/src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+//import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import ClienteBox from './components/cliente/ClienteBox';
+//import { Router, Route, Link } from 'react-router';
+import {BrowserRouter as Router, Route,Switch,Link} from 'react-router-dom';
+import Home from './components/home';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+        <Router>
+            <App>
+                <Switch>            
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/cliente" component={ClienteBox}/>
+                    {/*<Route path="/livro" component={LivroAdmin}/>*/}               
+                </Switch>            
+            </App>
+        </Router>
+    , document.getElementById('root'));
 registerServiceWorker();
