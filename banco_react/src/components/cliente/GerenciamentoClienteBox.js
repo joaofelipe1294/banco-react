b16f891cd1979807ca23b793229b5f74a20efb95
joao_felipe <joaofelipe1294@gmail.com>
2018-08-18 09:23:04 -0300

    Adicionado componente ListaCLiente ao componente GerenciadorClienteBox

--- banco_react/src/components/cliente/GerenciamentoClienteBox.js
+++ banco_react/src/components/cliente/GerenciamentoClienteBox.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import FormBuscaCliente from './FormBuscaCliente';
+import ListaCliente from './ListaCliente';
 
 export default class GerenciamentoClienteBox extends Component{
 
     render(){
         return (
-            <FormBuscaCliente/>
-
+            <div>
+                <FormBuscaCliente/>
+                <ListaCliente/>
+            </div>
         );
     }
 
